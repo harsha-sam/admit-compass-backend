ccbe1155ab00877aada58fa98b6889f39fe417ed
Harsha <harsha.sam23@gmail.com>
2024-12-08 17:47:59 -0500

    update tests

--- dist/middelware/clerkAuth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const withAuth = (req, res, next) => {
-    const { userId } = req.auth;
-    req.auth = userId ? auth : {};
-    next();
-};
-exports.default = withAuth;
